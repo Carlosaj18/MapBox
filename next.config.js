7509b4c689b8b9a3142ae1eec7340fa81c4f9a61
Charlyboy1993 <cjaramilloportilla@gmail.com>
2024-04-20 02:17:32 -0500

    Setup NextJs & tailwind & general css

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = {
+  images: {
+    // domains: ['upload.wikimedia.org']
+    domains: [
+      "links.papareact.com",
+      "jsonkeeper.com",
+      "api.npoint.io",
+      "www.nauticareport.it",
+      "a0.muscache.com",
+      "images.fineartamerica.com",
+      "i.ebayimg.com",
+      "www.thetimes.co.uk",
+    ],
+  },
+  nextConfig,
+  env: {
+    MAPBOX_KEY: process.env.NEXT_PUBLIC_MAPBOX_KEY,
+  },
+};
